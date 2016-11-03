e8dad05279395a0fed2fc6d6eb8f6196a7f95e33
ohadzr <ohadzr@gmail.com>
2016-11-03 19:26:21 +0200

    hw0 - part1 - not working

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+//
+// Created by ohad on 01-Nov-16.
+//
+
+#include <stdio.h>
+
+
+
+int main()
+{
+    printf("Hello world!\n");
+    for (int i=0; i<10;i++)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
