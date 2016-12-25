6350e75ac710f1a2b4688923c1a2778652798c48
SaharPaz <goldman@campus.technion.ac.il>
2016-12-25 12:02:51 +0200

    create location files

--- hw3/location_test.c
+++ hw3/location_test.c
@@ -2,4 +2,3 @@
 // Created by S on 25/12/2016.
 //
 
-#include "location_test.h"
--- hw3/location_test.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by S on 25/12/2016.
-//
-
-#ifndef MATAM_LOCATION_TEST_H
-#define MATAM_LOCATION_TEST_H
-
-#endif //MATAM_LOCATION_TEST_H
