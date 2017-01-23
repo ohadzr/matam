df2758caecde56c81b288f65ec20643fa2e7c00f
SaharPaz <goldman@campus.technion.ac.il>
2017-01-23 22:08:45 +0200

    items test done

--- hw4/tests/item_test.cpp
+++ hw4/tests/item_test.cpp
@@ -19,20 +19,29 @@ bool TestComboItem() {
     /********************************************************************
     *                              tests                               *
     ********************************************************************/
-    /* -------------------     test constractor    ------------------- */
 
-    ASSERT_NO_THROW( Item("POTION",1) );
-    ASSERT_NO_THROW( Item("CANDY",1) );
-    ASSERT_THROW( mtm::pokemongo::ItemInvalidArgsException , Item("Tomer",1) );
-    ASSERT_THROW( mtm::pokemongo::ItemInvalidArgsException , Item("CANDY",-1) );
-    ASSERT_THROW( mtm::pokemongo::ItemInvalidArgsException , Item("POTION",0) );
-
-    /* -------------------     test constractor    ------------------- */
+    ASSERT_NO_THROW(Item("POTION", 1));
+    ASSERT_NO_THROW(Item("CANDY", 1));
+    ASSERT_THROW(mtm::pokemongo::ItemInvalidArgsException, Item("Tomer", 1));
+    ASSERT_THROW(mtm::pokemongo::ItemInvalidArgsException, Item("CANDY", -1));
+    ASSERT_THROW(mtm::pokemongo::ItemInvalidArgsException, Item("POTION", 0));
 
 
+    Item item1 = Item("POTION", 1);
+    Item item2 = Item("CANDY", 2);
+    Item item3 = Item("POTION", 4);
+
+    Item item4(item2);
+    Item item5(item1);
+
+    ASSERT_EQUAL(item2.getLevel(), item4.getLevel());
+    ASSERT_EQUAL(item1.getType(), item5.getType());
+
+    return true;
+}
 
 
-    int main() {
+int main() {
     RUN_TEST(TestComboItem);
     return 0;
 }
